0b79ed8a82871d1f9556d8f7d2d2fd85ed359584
codax69 <pritbusiness03@gmail.com>
2025-10-11 01:49:13 +0530

    fix useAuth

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,35 +1,26 @@
 import React, { createContext, useState, useEffect, useContext } from "react";
 
-const AuthContext = createContext();
-// eslint-disable-next-line react-refresh/only-export-components
+export const AuthContext = createContext();
+
 export const useAuth = () => {
-  const context = useContext(AuthContext);
-  if (!context) {
-    throw new Error("useAuth must be used within an AuthProvider");
-  }
-  return context;
+  return useContext(AuthContext);
 };
 
-function AuthProvider({ children }) {
+export function AuthProvider({ children }) {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [token, setToken] = useState("");
 
   useEffect(() => {
     const accessToken = localStorage.getItem("token");
-    if (accessToken) {
+    if (accessToken === String) {
       setToken(accessToken);
       setIsLoggedIn(true);
     }
   }, []);
- 
 
   return (
-    <AuthContext.Provider
-      value={{ isLoggedIn, setIsLoggedIn, token}}
-    >
+    <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn, token, setToken }}>
       {children}
     </AuthContext.Provider>
   );
 }
-
-export { AuthProvider };
